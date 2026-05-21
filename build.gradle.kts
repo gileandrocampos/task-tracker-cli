551fc5644f7ae46cb029a7ae6f07f5ed42c7dcae
root <root@DESKTOP-574T64B.localdomain>
2026-05-20 21:46:56 -0300

    build: Initial config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    id("application")
 }
 
 group = "org.example"
@@ -15,6 +16,10 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass.set("MainKt")
 }
 
 tasks.test {
