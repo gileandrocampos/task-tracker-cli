5e420527f2dac261bfb46ca56936a3bfdc2e2230
gileandrocampos <gileandrocampos8@gmail.com>
2026-07-09 15:28:05 -0300

    feat: add structure to get args/commands to terminal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("Nenhum comando fornecido, digite !commands")
+        return
+    }
 
-    
+    val command = args[0].lowercase()
+    val inputData = args[1]
 
+    val storage = TaskStorage()
+    val manager = TaskManager(storage)
+
+    when (command) {
+        "add" -> {
+            val result = manager.add(inputData)
+            println("Tarefa adicionada com sucesso (ID: ${result.id})")
+        }
+    }
 }
