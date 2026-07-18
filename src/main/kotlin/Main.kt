dbd2f21dd0007030de090e1309fc491f54013663
gileandrocampos <gileandrocampos8@gmail.com>
2026-07-18 13:31:24 -0300

    feat: add new command for help user and obtained all parameters the user

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
-        println("Nenhum comando fornecido, digite !commands")
+        println("Nenhum comando fornecido, digite commands")
         return
     }
 
     val command = args[0].lowercase()
-    val inputData = args[1]
+    val inputData = args.copyOfRange(1, args.size).joinToString(" ")
 
     val storage = TaskStorage()
     val manager = TaskManager(storage)
@@ -14,6 +14,13 @@ fun main(args: Array<String>) {
         "add" -> {
             val result = manager.add(inputData)
             println("Tarefa adicionada com sucesso (ID: ${result.id})")
+        }
+        "commands" -> {
+            println("""
+                Comandos disponíveis:
+                - add <descrição>: Adiciona uma nova tarefa com a descrição fornecida.
+                - commands: Exibe a lista de comandos disponíveis.
+            """.trimIndent())
         }
     }
 }
